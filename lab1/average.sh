aa05ab2754c97f4d7349154789d790c9f6fe0b74
mjajksjjj <maks.myas5@gmail.com>
2020-03-07 07:25:51 +0000

    new av and num

--- lab1/average.sh
+++ lab1/average.sh
@@ -22,3 +22,4 @@ else
 	echo $#
 	echo $midle
 fi
+
--- lab1/numbers.sh
+++ lab1/numbers.sh
@@ -3,5 +3,6 @@ touch numbers.txt
 cp /dev/null numbers.txt
 for((i = 1; i <= $1; i++))
 do
- echo $RANDOM >> numbers.txt
+ #echo $RANDOM >> numbers.txt
+ echo $(od -A n -t d -N 1 < /dev/random) >> numbers.txt
 done
